b64b4e51ecdb15b610cdb6cab48570c62b8f36d9
ricardo <ricardo@HP-Pavilion>
2014-11-08 13:26:16 -0600

    Typo corrections on project description. From Netbeans IDE.

--- src/simpletree/Main.java
+++ src/simpletree/Main.java
@@ -1,9 +1,9 @@
 /**
- * This project is an school assigment, it's purpose is for me to learn.
+ * This project is an school assignment, it's purpose is for me to learn.
  * I'm learning how to create and use data structures called 'trees'.
  * In this specific case, I'm asked to build a tree to store a list of
  * patients with their names, gender (male, female), 
- * blood preasure (high, normal, low) and blood type(A, B, AB, O)
+ * blood pressure (high, normal, low) and blood type(A, B, AB, O)
  */
 package simpletree;
 
