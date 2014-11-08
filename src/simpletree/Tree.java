045cc4eb15e874e030365f911106091e66e7c149
ricardo <ricardo@HP-Pavilion>
2014-11-08 16:07:02 -0600

    This is the Tree interface that extends from InspectableTree and PositionalContainer.  * No more method definitions needed here.

---
+++ src/simpletree/Tree.java
@@ -0,0 +1,15 @@
+/*
+ * Free
+ * @author Jorge Ricardo Escobar Carrasco. * 
+ * AL12509938@unadmexico.mx * 
+ */
+package simpletree;
+
+/**
+ * This is the Tree interface that extends from InspectableTree and PositionalContainer.
+ * No more method definitions needed here.
+ * @author ricardo
+ */
+public interface Tree extends InspectableTree, PositionalContainer {
+    
+}
