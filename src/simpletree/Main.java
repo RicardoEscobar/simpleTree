b4d5ba40e586209cdcd5cf2dd982b29fb1f548a5
ricardo <ricardo@HP-Pavilion>
2014-11-08 12:48:16 -0600

    First commit. From Netbeans IDE.

---
+++ src/simpletree/Main.java
@@ -0,0 +1,24 @@
+/**
+ * This project is an school assigment, it's purpose is for me to learn.
+ * I'm learning how to create and use data structures called 'trees'.
+ * In this specific case, I'm asked to build a tree to store a list of
+ * patients with their names, gender (male, female), 
+ * blood preasure (high, normal, low) and blood type(A, B, AB, O)
+ */
+package simpletree;
+
+/**
+ *
+ * @author Jorge Ricardo Escobar Carrasco. AL12509938@unadmexico.mx 
+ */
+public class Main {
+
+    /**
+     * I'm using this Main class to execute the methods I create for the assigment.
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello.");
+    }
+    
+}
